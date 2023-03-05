bf4df2e92733825cb4a25d434d1e65eb6ec3ae26
henry232323 <14969260+henry232323@users.noreply.github.com>
2023-03-04 20:27:46 -0700

    Attempted fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import {useState} from 'react'
 import './App.css'
 import {ExampleMessenger} from "./components/messaging";
 
-function App() {
-    const [count, setCount] = useState(0)
 
+function App() {
     return (
         <ExampleMessenger/>
     )
