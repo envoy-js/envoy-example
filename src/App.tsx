4b56b820d02ab1e6ce389c1250b89e0017f4c9ba
henry232323 <14969260+henry232323@users.noreply.github.com>
2023-03-05 10:24:36 -0700

    Example read

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import {ExampleMessenger} from "./components/messaging";
 
 
--- src/components/messaging.tsx
+++ src/components/messaging.tsx
@@ -1,17 +1,19 @@
 import {ChatServerProvider, Messenger} from "@envoy-js/react";
 
-import React, {useEffect, useState} from "react";
-import {Button, Container, Grid, Paper, TextField, Typography} from "@mui/material";
+import React, {useEffect, useMemo, useState} from "react";
+import {Avatar, Button, Container, Grid, Paper, TextField, Typography} from "@mui/material";
 
 interface User {
     username: string
-    id: string
+    id: string,
+    avatar: string
 }
 
 interface Message {
     id: string
     value: string
-    roomid: string
+    roomid: string,
+    author: User
 }
 
 interface Room {
@@ -19,7 +21,7 @@ interface Room {
     users: User[]
 }
 
-const messenger = new Messenger<Message, Room>(
+const messenger = new Messenger<Message, Room, User>(
     "ws://localhost:3000",
     "id",
     (m) => m.roomid
@@ -37,21 +39,48 @@ export function MessagesArea() {
         console.log(rooms)
     }, [createRoom, rooms])
 
-    return <Container maxWidth={'md'}>
-        <Grid container spacing={4}>
-            {(rooms || []).map(r => <ExampleChat room={r.room}/>)}
-        </Grid>
-    </Container>
+    return <main style={{width: '100vw', height: '100vh', marginLeft: 'auto', marginRight: 'auto'}}>
+        <Container maxWidth={'md'}>
+            <Grid container spacing={4}>
+                {(rooms || []).map((r, i) => <ExampleChat room={r.room} key={i}/>)}
+            </Grid>
+        </Container>
+    </main>
 }
 
 
 export function MessageEntry(props: { message: Message }) {
-    return <Grid item xs={12}>
-        <Paper color={'primary'}>
+    const {me} = messenger.useChatServer()
+    const {message} = props
+    const isMine = useMemo(() => (message.author.username === me?.username), [message, me])
+
+    useEffect(() => {
+        console.log("Msgs", message.author.username, me?.username)
+    }, [me, message])
+
+    return <Grid item xs={12} display={'flex'}
+                 justifyContent={isMine ? 'right' : 'left'}>
+        {!isMine &&
+            <Avatar src={props.message.author.avatar} variant={'rounded'} sx={{width: 48, height: 48}}/>
+        }
+        <Paper sx={{
+            padding: 1,
+            // minWidth: 250,
+            backgroundColor: isMine ? '#78cbde' : '#FFF',
+            display: 'flex',
+            justifyContent: isMine ? 'right' : 'left',
+            marginRight: 1,
+            marginLeft: 1,
+            alignItems: 'center',
+            maxWidth: 450,
+            overflowWrap: 'anywhere'
+        }} variant="outlined">
             <Typography>
                 {props.message.value}
             </Typography>
         </Paper>
+        {isMine &&
+            <Avatar src={props.message.author.avatar} variant={'rounded'} sx={{width: 48, height: 48}}/>}
     </Grid>
 }
 
@@ -64,19 +93,20 @@ export function ExampleChat(props: { room: { id: string } }) {
     }, [sendMessage, messages])
 
     return <Grid item xs={12}>
-        <Paper>
-            <Grid container justifyContent={'space-between'} direction={'column'}>
-                <Grid item>
+        <Paper sx={{width: '100%', padding: 2, maxHeight: '100vh', overflow: "scroll"}} variant="outlined">
+            <Grid container justifyContent={'space-between'} direction={'column'} spacing={2} sx={{minHeight: 500}}>
+                <Grid item xs={12}>
                     <Grid container spacing={1}>
-                        {(messages || []).map(m => <MessageEntry message={m}/>)}
+                        {(messages || []).map((m, i) => <MessageEntry message={m} key={i}/>)}
                     </Grid>
                 </Grid>
-                <Grid item display={'flex'} justifyContent={'space-between'}>
-                    <TextField fullWidth value={content} onChange={(event) => setContent(event.target.value)}/>
+                <Grid item xs={12} display={'flex'} justifyContent={'space-between'}>
+                    <TextField fullWidth value={content} onChange={(event) => setContent(event.target.value)}
+                               multiline/>
                     <Button onClick={(event) => {
                         sendMessage!({value: content, roomid: props.room.id})
                         setContent("")
-                    }}>
+                    }} sx={{marginLeft: 2}} variant={'contained'}>
                         Submit
                     </Button>
                 </Grid>
